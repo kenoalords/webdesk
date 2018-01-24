c70d1fc3227dc8e79338c14a6c3b23d39d755d8c
vagrant <kenoalords@gmail.com>
2018-01-24 11:44:40 +0000

    google analytic code corrected

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -27,6 +27,8 @@
         </div>
         
         <script src="{{ asset('js/app.js') }}"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112666247-1"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
