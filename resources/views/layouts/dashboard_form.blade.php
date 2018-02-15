dfb5532810d10a6a2fa3e1c1c4a0f24b2ff89f3e
vagrant <kenoalords@gmail.com>
2018-02-15 14:03:32 +0000

    added google adwords tag manager

--- resources/views/layouts/dashboard_form.blade.php
+++ resources/views/layouts/dashboard_form.blade.php
@@ -62,5 +62,15 @@
             gtag('js', new Date());
             gtag('config', 'UA-112666247-1');
         </script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114100486-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-114100486-1');
+        </script>
+
     </body>
 </html>
